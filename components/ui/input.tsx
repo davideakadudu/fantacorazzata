374aed245358c1b1253870966de4052232ebae0f
davideakadudu <72854390+davideakadudu@users.noreply.github.com>
2025-09-07 12:58:16 +0200

    Delete components/ui directory

--- components/ui/input.tsx
+++
@@ -1,5 +0,0 @@
-
-import * as React from "react";
-export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(function Inp({ className="", ...props}, ref){
-  return <input ref={ref} className={`h-9 px-3 border rounded-md w-full ${className}`} {...props} />;
-});
